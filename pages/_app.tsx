4d2542ddc54f7e867f6c4e4922cd684c6020fb89
girihanbudi <Ghanbudi@gmail.com>
2023-03-15 23:11:06 +0700

    wrap app with redux provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,16 @@ import lightTheme from "@/styles/themes/lightTheme";
 import { ThemeProvider } from "@mui/material/styles";
 import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
+import { Provider } from "react-redux";
+import { wrapper, store } from "@/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={lightTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
