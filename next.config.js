1dd7cbf3e170f347146456513a9bc49ef4152dfd
girihanbudi <Ghanbudi@gmail.com>
2023-03-15 23:11:25 +0700

    fixed warning nextconfig

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require('./next-i18next.config')
 
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: { appDir: false },
-}
-
 module.exports = {
   i18n,
-  nextConfig,
+  reactStrictMode: true,
+  experimental: { appDir: false },
   images: {
     remotePatterns: [
       {
