ffa273bcae34c7a662295b8644c34db20d3ed7a7
girihanbudi <Ghanbudi@gmail.com>
2023-03-16 23:55:26 +0700

    updated graphql configuration
    - changed method to GET
    - added server cookies readability
    - changed client const name to be able multiple graph

--- libs/apollo-client.ts
+++ libs/apollo-client.ts
@@ -1,36 +1,10 @@
-// ./apollo-client.js
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
-import {
-  ApolloClient,
-  ApolloLink,
-  HttpLink,
-  InMemoryCache,
-} from "@apollo/client";
-
-type ClientName = "main" | "other";
-
-const mainServiceClient = new HttpLink({
-  uri: `${process.env.NEXT_PUBLIC_BACKEND}/graph`,
-});
-
-var thirdPartyServiceClient = new HttpLink({
-  uri: `${process.env.NEXT_PUBLIC_BACKEND}/graph`,
-});
-
-export const changeThirdPartyLink = (newLink: string) => {
-  thirdPartyServiceClient = new HttpLink({
-    uri: newLink,
-  });
-};
-
-export const client = new ApolloClient({
-  // link: ApolloLink.split(
-  //   (operation) => operation.getContext().clientName === "main",
-  //   mainServiceClient, //if above
-  //   thirdPartyServiceClient
-  // ),
-  uri: `${process.env.NEXT_PUBLIC_BACKEND}/graph`,
+export const userClient = new ApolloClient({
+  link: new HttpLink({
+    uri: `${process.env.NEXT_PUBLIC_BACKEND}/graph`,
+    useGETForQueries: true,
+  }),
   cache: new InMemoryCache(),
+  credentials: "include",
 });
-
-export default client;
