6d0f2e1d188222cd3e6327ff7dbddbd4989e56a6
girihanbudi <Ghanbudi@gmail.com>
2023-03-17 23:24:05 +0700

    added types exporter

---
+++ modules/Setting/types/index.ts
@@ -0,0 +1,5 @@
+export type { ILocale } from "./ilocale";
+export { ilocaleKeys, defaultLocale } from "./ilocale";
+
+export type { ICurrency } from "./icurrency";
+export { icurrencyKeys, defaultCurrency } from "./icurrency";
