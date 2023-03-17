4f95c9a5fb013018c7a7cd77af88114613960467
girihanbudi <Ghanbudi@gmail.com>
2023-03-17 23:24:42 +0700

    moved common error message to translation

--- common/error-code.ts
+++ common/error-code.ts
@@ -1,46 +1,20 @@
 // Error Formatting Template for app
 export interface ErrorCode {
   key: string; // this used for error naming code
-  errorEn: Error; // this is the error message in english
   code: number; // this is error code for http response code
-}
-
-// Get all error key and message into object for translation
-export function getTranslation(object: any, translation: string = "En"): {} {
-  const objs: { [key: string]: any } = {};
-  Object.entries(object).forEach(([key, value]) => {
-    const val = value as ErrorCode;
-    const keyName = `error${translation}` as keyof ErrorCode;
-
-    objs[key] = val[keyName];
-  });
-  return objs;
 }
 
 // Default error
 export class DefaultError implements ErrorCode {
-  static readonly DEFAULT_FED_001 = new DefaultError(
-    "DEFAULT_FED_001",
-    new Error("Field cannot be empty"),
-    400
-  );
+  static readonly DEFAULT_FED_001 = new DefaultError("DEFAULT_FED_001", 400);
 
-  static readonly DEFAULT_SYS_500 = new DefaultError(
-    "DEFAULT_SYS_500",
-    new Error("Internal Server Error"),
-    500
-  );
+  static readonly DEFAULT_SYS_500 = new DefaultError("DEFAULT_SYS_500", 500);
 
-  static readonly DEFAULT_SCM_001 = new DefaultError(
-    "DEFAULT_SCM_001",
-    new Error("Bad schema request"),
-    400
-  );
+  static readonly DEFAULT_SCM_001 = new DefaultError("DEFAULT_SCM_001", 400);
 
   // private to disallow creating other instances of this type
   private constructor(
     public readonly key: string,
-    public readonly errorEn: Error,
     public readonly code: number
   ) {}
 }
