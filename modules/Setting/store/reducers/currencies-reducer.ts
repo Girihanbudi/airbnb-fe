d32a47dbcc534263a01de5041d366fa91ff4d1f0
girihanbudi <Ghanbudi@gmail.com>
2023-03-17 23:23:35 +0700

    added currencies reducer

---
+++ modules/Setting/store/reducers/currencies-reducer.ts
@@ -0,0 +1,47 @@
+import ICurrency from "../../types/icurrency";
+import { DefaultError } from "@/common/error-code";
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchCurrenciesThunk } from "../actions/gql-async-thunk";
+
+export interface ICurrenciesStore {
+  loading: boolean;
+  data: ICurrency[] | null;
+  error?: string;
+  errorCode?: string;
+  rejected: boolean;
+}
+
+const initialCurrenciesStore: ICurrenciesStore = {
+  loading: true,
+  data: null,
+  rejected: false,
+};
+
+export const currenciesSlice = createSlice({
+  name: "currencies",
+  initialState: initialCurrenciesStore,
+  reducers: {
+    // standard reducer logic, with auto-generated action types per reducer
+  },
+  extraReducers: (builder) => {
+    // Add reducers for additional action types here, and handle loading state as needed
+    builder.addCase(fetchCurrenciesThunk.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchCurrenciesThunk.fulfilled, (state, action) => {
+      state.loading = false;
+      state.data = action.payload.data.currencies;
+      state.error = action.payload.error
+        ? action.payload.error.message
+        : undefined;
+    });
+    builder.addCase(fetchCurrenciesThunk.rejected, (state) => {
+      state.loading = false;
+      state.data = null;
+      state.error = DefaultError.DEFAULT_SYS_500.key;
+      state.rejected = true;
+    });
+  },
+});
+
+export default currenciesSlice.reducer;
