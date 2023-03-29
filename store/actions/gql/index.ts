28a9838a001d07b2e9793d9810e8ad5f29b17d46
girihanbudi <Ghanbudi@gmail.com>
2023-03-29 20:07:26 +0700

    added graphql function exporter

---
+++ store/actions/gql/index.ts
@@ -0,0 +1,6 @@
+export { fetchLocales } from "./fetch-locales";
+export { fetchCurrencies } from "./fetch-currencies";
+export { fetchCountries } from "./fetch-countries";
+export type { Payload as FetchLocalesPayload } from "./fetch-locales";
+export type { Payload as FetchCurrenciesPayload } from "./fetch-currencies";
+export type { Payload as FetchCountriesPayload } from "./fetch-countries";
