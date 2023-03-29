37faa648c12c7f3a9195edc42e86d410ccb4f256
girihanbudi <Ghanbudi@gmail.com>
2023-03-29 20:08:04 +0700

    refactor types

--- modules/Setting/types/index.ts
+++
@@ -1,5 +0,0 @@
-export type { ILocale } from "./ilocale";
-export { ilocaleKeys, defaultLocale } from "./ilocale";
-
-export type { ICurrency } from "./icurrency";
-export { icurrencyKeys, defaultCurrency } from "./icurrency";
