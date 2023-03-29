055fcbb7b6dcb2a089b70f2f62673aca45749953
girihanbudi <Ghanbudi@gmail.com>
2023-03-29 20:05:49 +0700

    added types exporter

---
+++ types/index.ts
@@ -0,0 +1,9 @@
+export { ipaginationKeys, defaultPagination } from "./ipagination";
+export { icurrencyKeys, defaultCurrency } from "./icurrency";
+export { ilocaleKeys, defaultLocale } from "./ilocale";
+export { icountryKeys, defaultCountry } from "./icountry";
+
+export type { IPagination } from "./ipagination";
+export type { ICurrency } from "./icurrency";
+export type { ILocale } from "./ilocale";
+export type { ICountry } from "./icountry";
