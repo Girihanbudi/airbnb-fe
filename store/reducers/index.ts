14e8bfa2e1ba3d6de9234c9476023e0ef4df02ac
girihanbudi <Ghanbudi@gmail.com>
2023-03-29 20:09:35 +0700

    added reducer exporter

---
+++ store/reducers/index.ts
@@ -0,0 +1,3 @@
+export { default as localesReducer } from "./locales-reducer";
+export { default as currenciesReducer } from "./currencies-reducer";
+export { default as countriesReducer } from "./countries-reducer";
