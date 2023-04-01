fc740c1cb7f0ea5541bb1df5da3786fb23282ecc
girihanbudi <Ghanbudi@gmail.com>
2023-04-01 14:33:25 +0700

    updated countries thunk
    - fixed wrong name thunk
    - added fetch more countries event thunk
    - added pagination meta

--- store/reducers/countries-reducer.ts
+++ store/reducers/countries-reducer.ts
@@ -1,12 +1,15 @@
 import { DefaultError } from "@/common";
 import { createSlice } from "@reduxjs/toolkit";
-import { IPagination, ICountry } from "@/types";
-import { fetchCountriesThunk } from "../actions/thunk/gql-async-thunk";
+import { IPagination, ICountry, defaultPagination } from "@/types";
+import {
+  fetchCountriesThunk,
+  fetchMoreCountriesThunk,
+} from "../actions/thunk/gql-async-thunk";
 
 export interface ICountriesStore {
   loading: boolean;
   data: ICountry[] | null;
-  meta?: IPagination | null;
+  meta: IPagination | null;
   error?: string;
   errorCode?: string;
   rejected: boolean;
@@ -15,6 +18,7 @@ export interface ICountriesStore {
 const initialCountriesStore: ICountriesStore = {
   loading: true,
   data: null,
+  meta: null,
   rejected: false,
 };
 
@@ -25,7 +29,7 @@ export const countriesSlice = createSlice({
     // standard reducer logic, with auto-generated action types per reducer
   },
   extraReducers: (builder) => {
-    // Add reducers for additional action types here, and handle loading state as needed
+    // Initial fetch countries
     builder.addCase(fetchCountriesThunk.pending, (state) => {
       state.loading = true;
     });
@@ -40,6 +44,30 @@ export const countriesSlice = createSlice({
     builder.addCase(fetchCountriesThunk.rejected, (state) => {
       state.loading = false;
       state.data = null;
+      state.error = DefaultError.DEFAULT_SYS_500.key;
+      state.rejected = true;
+    });
+
+    // Fetch more countries if any
+    builder.addCase(fetchMoreCountriesThunk.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchMoreCountriesThunk.fulfilled, (state, action) => {
+      state.loading = false;
+      const newData = action.payload.data.countries;
+      if (newData) {
+        if (state.data && newData.data)
+          state.data = [...state.data, ...newData.data];
+        else if (newData.data) state.data = newData.data;
+        state.meta = action.payload.data.countries.meta;
+      }
+
+      state.error = action.payload.error
+        ? action.payload.error.message
+        : undefined;
+    });
+    builder.addCase(fetchMoreCountriesThunk.rejected, (state) => {
+      state.loading = false;
       state.error = DefaultError.DEFAULT_SYS_500.key;
       state.rejected = true;
     });
