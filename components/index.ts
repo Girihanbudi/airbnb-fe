da9021d6590e7b6ac47a37ed196bd43252c805e0
girihanbudi <Ghanbudi@gmail.com>
2023-04-17 23:00:24 +0700

    added component exporter

---
+++ components/index.ts
@@ -0,0 +1,25 @@
+export { AlignedButtonIcon } from "./AlignedButtonIcon";
+export { AnimatedGradientButton } from "./AnimatedGradientButton";
+export { AnimatedUnderline } from "./AnimatedUnderline";
+export { BestWidth } from "./BestWidth";
+export { CardButton } from "./CardButton";
+export { CardMenu } from "./CardMenu";
+export {
+  Carousel,
+  CarouselNavButton,
+  CarouselNavButtonPrev,
+  CarouselNavButtonNext,
+} from "./Carousel";
+export { ElevationScroll } from "./ElevationScroll";
+export { DefaultError } from "./Error";
+export { OptionButton, OptionButtonLoader } from "./OptionButton";
+export { OutlinedSelectField } from "./OutlinedSelectField";
+export { OutlinedTextField } from "./OutlinedTextField";
+export { PlainButton } from "./PlainButton";
+export { PlainLink } from "./PlainLink";
+export { SlideDialog } from "./SlideDialog";
+export { Tabs, TabHeader, TabPanel } from "./Tabs";
+export { TextButton } from "./TextButton";
+export { Slide } from "./Transition";
+export { UnderlinedButton } from "./UnderlinedButton";
+export { UnderlinedText } from "./UnderlinedText";
