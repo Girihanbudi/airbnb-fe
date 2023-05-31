c5253acd3b2ad5517497e43989862b0571a85bbf
girihanbudi <Ghanbudi@gmail.com>
2023-05-31 20:40:41 +0700

    changed static locales data behavior

---
+++ public/locales/en/translation.ts
@@ -0,0 +1,46 @@
+export const Translations = {
+  // Page Translations
+  pageIndex: "Vacation Homes & Condos Rental - Airbnb",
+
+  // Text translations
+  txtLoginSignup: "Log in or sign up",
+  txtWelcomeToAirbnb: "Welcome to Airbnb",
+  txtContinueOr: "or",
+  txtRateWarningPhone:
+    "We’ll call or text you to confirm your number. Standard message and data rates apply",
+  txtPrivacyPolicy: "Privacy Policy",
+  txtAirbnbSetup: "Airbnb your home",
+  txtChooseLanguageAndRegion: "Choose a language and region",
+  txtChooseCurrency: "Choose a currency",
+  txtSignUp: "Sign up",
+  txtLogIn: "Log in",
+  txtLogOut: "Log out",
+  txtAccount: "Account",
+  txtHostAnExperience: "Host an experience",
+  txtMessages: "Messages",
+  txtTrips: "Trips",
+  txtWishlists: "Wishlists",
+  txtReferAHost: "Refer a host",
+  txtHelp: "Help",
+
+  // Button translations
+  btnContinueWithGoogle: "Continue with Google",
+  btnContinueWithFacebook: "Continue with Facebook",
+  btnContinueWithEmail: "Continue with Email",
+  btnContinueWithPhone: "Continue",
+  btnWhere: "Anywhere",
+  btnWhen: "Anyweek",
+  btnGuest: "Add guest",
+  btnFilter: "Filter",
+
+  // Tab translations
+  tabLocale: "Language and Region",
+  tabCurrency: "Currency",
+
+  // Error code translations
+  DEFAULT_FED_001: "Field cannot be empty",
+  DEFAULT_SYS_500: "Internal server error",
+  DEFAULT_SCM_001: "Bad schema request",
+};
+
+export default Translations;
---
+++ public/locales/id/translation.ts
@@ -0,0 +1,46 @@
+export const Translations = {
+  // Page Translations
+  pageIndex: "Sewa rumah untuk liburan & Kondominium - Airbnb",
+
+  // Text translations
+  txtLoginSignup: "Masuk atau mendaftar",
+  txtWelcomeToAirbnb: "Selamat Datang di Airbnb",
+  txtContinueOr: "atau",
+  txtRateWarningPhone:
+    "Kami akan menelepon atau mengirim SMS guna mengonfirmasikan nomor Anda. Tarif standar SMS dan data berlaku",
+  txtPrivacyPolicy: "Kebijakan Privasi",
+  txtAirbnbSetup: "Jadikan rumah anda Airbnb",
+  txtChooseLanguageAndRegion: "Pilih bahasa dan wilayah",
+  txtChooseCurrency: "Pilih mata uang",
+  txtSignUp: "Daftar",
+  txtLogIn: "Masuk",
+  txtLogOut: "Keluar",
+  txtAccount: "Akun",
+  txtHostAnExperience: "Adakan pengalaman",
+  txtMessages: "Pesan",
+  txtTrips: "Perjalanan",
+  txtWishlists: "Favorit",
+  txtReferAHost: "Rekomendasikan tuan rumah",
+  txtHelp: "Bantuan",
+
+  // Button translations
+  btnContinueWithGoogle: "Lanjutkan dengan Google",
+  btnContinueWithFacebook: "Lanjutkan dengan Facebook",
+  btnContinueWithEmail: "Lanjutkan dengan Email",
+  btnContinueWithPhone: "Lanjutkan",
+  btnWhere: "Ke mana pun",
+  btnWhen: "Minggu kapan pun",
+  btnGuest: "Tambahkan tamu",
+  btnFilter: "Filter",
+
+  // Tab translations
+  tabLocale: "Bahasa dan Wilayah",
+  tabCurrency: "Mata Uang",
+
+  // Error code translations
+  DEFAULT_FED_001: "Field tidak boleh kosong",
+  DEFAULT_SYS_500: "Terjadi kesalahan server",
+  DEFAULT_SCM_001: "Skema permintaan tidak sesuai",
+};
+
+export default Translations;
