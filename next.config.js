8eeb2e33a50075087aac733d908459663ecba303
girihanbudi <Ghanbudi@gmail.com>
2023-05-31 20:43:16 +0700

    moved i18n as reducer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
-const { i18n } = require('./next-i18next.config')
 
 module.exports = {
-  i18n,
   reactStrictMode: true,
   experimental: { appDir: false },
   images: {
